e68976a9a7faeeefa7eb23a6979eb93e36d678a9
Dmitry Pavlenko <104529636+jokino9212@users.noreply.github.com>
2024-04-29 16:51:56 +0300

    data with reap api

--- script.js
+++ script.js
@@ -1,63 +1,68 @@
-const data = {"sources":[{"id":1,"name":"meduza"},{"id":6,"name":"3dnews"},{"id":7,"name":"nytimes"},{"id":8,"name":"forbes"},{"id":9,"name":"igromania"},{"id":10,"name":"buro237"},{"id":11,"name":"rusvesna"},{"id":12,"name":"7ya"},{"id":14,"name":"aif"},{"id":15,"name":"gazetaru"},{"id":16,"name":"karpov.courses"}],"categories":[{"id":1,"name":"tech"},{"id":2,"name":"sport"},{"id":3,"name":"fashion"},{"id":4,"name":"politics"},{"id":5,"name":"other"},{"id":6,"name":"karpov.courses"}],"items":[{"id":537364,"lang":"ru","date":"Mon Apr 15 2024 20:24:00 GMT+0300 (Moscow Standard Time)","title":"Представлены Realme P1 и P1 Pro — смартфоны среднего уровня с AMOLED-дисплеями и 50-Мп камерами","description":"Компания Realme представила первые смартфоны серии P — аппараты Realme P1 и P1 Pro. Оба устройства оснащены дисплеями AMOLED с частотой обновления 120 Гц, основными камерами на базе 50-Мп сенсоров и аккумуляторными батареями ёмкостью 5000 мА·ч.","image":"https://3dnews.ru/assets/external/illustrations/2024/04/15/1103311/489.jpg","source_id":6,"category_id":1},{"id":537326,"lang":"ru","date":"Mon Apr 15 2024 20:23:18 GMT+0300 (Moscow Standard Time)","title":"В глазу съевшей крокодила девушки поселился редкий паразит","description":"Пациентка два года ходила с мигрирующим паразитом в глазу.","image":"https://aif-s3.aif.ru/images/036/647/7fa5729d738ab364ba9afb05b2afb2f4.jpg","source_id":14,"category_id":4},{"id":537334,"lang":"ru","date":"Mon Apr 15 2024 20:20:38 GMT+0300 (Moscow Standard Time)","title":"РИА Новости: в правительстве одобрили проект о праве приставов на оружие","description":"С инициативой разрешить всем сотрудникам ФССП носить огнестрельное оружие и спецсредства выступил Минюст.","image":"https://aif-s3.aif.ru/images/036/647/5b2bef98a3ea6b3cfdbb3e27d10c6ade.png","source_id":14,"category_id":4},{"id":537327,"lang":"ru","date":"Mon Apr 15 2024 20:20:38 GMT+0300 (Moscow Standard Time)","title":"Стратегия Sweet Transit покинет ранний доступ 22 апреля","description":"","image":"https://newcdn.igromania.ru/articles/pics/tmp/images/2024/4/15/7efb6733-7eb6-437e-abf1-b72819d9b743.jpg","source_id":9,"category_id":1},{"id":537373,"lang":"ru","date":"Mon Apr 15 2024 20:15:53 GMT+0300 (Moscow Standard Time)","title":"В Минсельхозе заявили, что повышение акцизов не должно влиять на подорожание вина","description":"Грядущее повышение акцизов на винодельческую продукцию не должно повлиять на потребительские цены, так как налоговая нагрузка на производителей не изменится. Об этом сообщает ТАСС со ссылкой на пресс-службу Минсельхоза России.\n\n\"Как и ...","image":"https://img.gazeta.ru/files3/903/12577903/Depositphotos_12158570_xl-2015-pic905-895x505-73338.jpg","source_id":15,"category_id":4},{"id":537324,"lang":"ru","date":"Mon Apr 15 2024 20:15:50 GMT+0300 (Moscow Standard Time)","title":"Samsung получит от властей США $6,4 млрд на расширение производства чипов в Техасе","description":"Samsung получит от властей США до $6,4 млрд на расширение производства чипов в штате Техас. Финансирование пойдет на создание и работу двух заводов, а также центры исследований и упаковки чипов в городе Тейлор. В прошлом месяце Intel получила от американских властей $8,5 млрд на строительство нескольких заводов по производству микросхем в США","image":"","source_id":8,"category_id":5},{"id":537342,"lang":"ru","date":"Mon Apr 15 2024 20:15:23 GMT+0300 (Moscow Standard Time)","title":"На Украине сотрудники военкомата бросили задержанного мужчину в лесу","description":"Задержанного украинскими военкомами мужчину нашли в лесу пограничники, они намерены выписать ему штраф.","image":"https://aif-s3.aif.ru/images/036/647/daca8e457d4b5ef6c1fb6b8adf255a64.jpg","source_id":14,"category_id":5},{"id":537349,"lang":"ru","date":"Mon Apr 15 2024 20:15:03 GMT+0300 (Moscow Standard Time)","title":"Зеленский потребовал от Запада защитить Украину, как Израиль","description":"Президент Украины Владимир Зеленский призвал западных политиков защищать Украину не менее яростно, чем Израиль во время атаки Ирана. Об этом глава государства написал в своем Telegram-канале.\n\n\"Защитив Израиль, свободный мир ...","image":"https://img.gazeta.ru/files3/365/18329365/2024-02-25T160521Z_103070820_RC2Q96AC5APP_RTRMADP_3_UKRAINE-CRISIS-ZELENSKIY__1_-pic905-895x505-30510.jpg","source_id":15,"category_id":4},{"id":537346,"lang":"ru","date":"Mon Apr 15 2024 20:15:00 GMT+0300 (Moscow Standard Time)","title":"Риттер допустил завершение конфликта на Украине в течение года","description":"Армия РФ силы наращивает, а боеспособность ВСУ падает, отметил американский эксперт.","image":"https://aif-s3.aif.ru/images/036/647/dc19ccaf22a81b34861980aada56e288.jpg","source_id":14,"category_id":5},{"id":537376,"lang":"ru","date":"Mon Apr 15 2024 20:09:25 GMT+0300 (Moscow Standard Time)","title":"В России ввели токены на квадратные метры коммерческой недвижимости ","description":"На платформе \"А-Токен\" Альфа-Банка впервые токенизировали квадратные метры коммерческой недвижимости - торгово-развлекательного центра KazanMall. Эмитентом в сделке выступил владелец ТРЦ - девелоперский холдинг ...","image":"https://img.gazeta.ru/files3/807/18451807/4K8A6067-pic905-895x505-94894.jpg","source_id":15,"category_id":4},{"id":537382,"lang":"ru","date":"Mon Apr 15 2024 20:05:59 GMT+0300 (Moscow Standard Time)","title":"Шоколад помогает перестать кусочничать, рассказала врач","description":"Американский диетолог Эмма Бардвелл рассказала изданию Daily Mail, что несколько кусочков шоколада во второй половине дня после еды помогают избежать вечернего \"кусочничества\". Такой метод контроля аппетита полезен для людей с ...","image":"https://img.gazeta.ru/files3/227/16463227/shutterstock_1613544322-pic905-895x505-65373.jpg","source_id":15,"category_id":1},{"id":537348,"lang":"ru","date":"Mon Apr 15 2024 20:04:35 GMT+0300 (Moscow Standard Time)","title":"Жительница Орска показала свой дом после наводнения","description":"В комнатах вздулись стеновые панели, слетели с петель двери.","image":"https://aif-s3.aif.ru/images/036/647/7d9cf5a5506a9e39e7da3a2f05daa040.jpg","source_id":14,"category_id":5},{"id":537337,"lang":"ru","date":"Mon Apr 15 2024 20:02:30 GMT+0300 (Moscow Standard Time)","title":"Разработчик сделал предложение с помощью игры для консоли Playdate","description":"","image":"https://newcdn.igromania.ru/articles/pics/tmp/images/2024/4/15/b80b969c-d62c-4472-b8da-dc806ceed92c.jpg","source_id":9,"category_id":1},{"id":537351,"lang":"ru","date":"Mon Apr 15 2024 19:57:00 GMT+0300 (Moscow Standard Time)","title":"«Металлург» и «Локомотив» встретятся в финале Кубка Гагарина-2024","description":"В Магнитогорске завершился решающий матч полуфинальной серии плей-офф КХЛ.","image":"https://aif-s3.aif.ru/images/036/647/e79d2d98ecb002598e94f98a56d477a3.jpg","source_id":14,"category_id":5},{"id":537352,"lang":"ru","date":"Mon Apr 15 2024 19:53:41 GMT+0300 (Moscow Standard Time)","title":"Американский профессор назвал приглашение Украины в НАТО катастрофической ошибкой","description":"Принятое в 2008 году решение о приглашении Украины в НАТО привело к катастрофическим последствиям, которые будут ощущаться в мире еще долго. Такое мнение в эфире Youtube-канала Daniel Davis / Deep Dive выразил американский профессор Джон ...","image":"https://img.gazeta.ru/files3/155/17855155/2-pic905-895x505-41897.jpg","source_id":15,"category_id":4},{"id":537354,"lang":"ru","date":"Mon Apr 15 2024 19:40:42 GMT+0300 (Moscow Standard Time)","title":"В Дагестане завели дело против водителя, упавшего с детьми в канал","description":"В результате ДТП погиб ребенок.","image":"https://aif-s3.aif.ru/images/036/647/e082bca361c2708d1faca0e8d5ff778e.jpg","source_id":14,"category_id":5},{"id":537356,"lang":"ru","date":"Mon Apr 15 2024 19:39:17 GMT+0300 (Moscow Standard Time)","title":"Бастрыкин поручил выяснить, почему уволили врача, пришившего лицо ребенку","description":"Уникальная операция была проведена в Краснодаре под руководством хирурга Алексея Дикарева.","image":"https://aif-s3.aif.ru/images/036/647/e16c2aa28c06cf446d5f9b7b62ee01f8.jpg","source_id":14,"category_id":5},{"id":537331,"lang":"ru","date":"Mon Apr 15 2024 19:38:58 GMT+0300 (Moscow Standard Time)","title":"В Минпросвещения объяснили включение гольфа в школьную программу по физкультуре","description":"Министерство просвещения включило в школьную программу по физкультуре гольф, чирлидинг, роллер-спорт и городошный спорт. В ведомстве среди прочего отметили, что занятия гольфом сформируют у обучающихся чувство патриотизма и нравственные качества","image":"","source_id":8,"category_id":5},{"id":537329,"lang":"ru","date":"Mon Apr 15 2024 19:38:33 GMT+0300 (Moscow Standard Time)","title":"Блогера Портнягина отправили под домашний арест по делу о неуплате налогов","description":"Блогера Дмитрия Портнягина отправили под домашний арест до 10 июня по делу об уклонении от налогов и отмывании денег. По версии следствия, задолженность Портнягина и его жены по уплате налогов превысила 124 млн рублей, а пени и штрафы — 64 млн рублей. 12 апреля блогера задержали в Ростове-на-Дону и доставили на допрос в СК","image":"","source_id":8,"category_id":5},{"id":537359,"lang":"ru","date":"Mon Apr 15 2024 19:34:30 GMT+0300 (Moscow Standard Time)","title":"Суд отправил блогера Портнягина под домашний арест до 10 июня","description":"Блогер Портнягин будет отбывать домашний арест в своем загородном доме в Московской области.","image":"https://aif-s3.aif.ru/images/036/647/c8f6afc3813074b57e58bb0b00e1378c.png","source_id":14,"category_id":4}]}
-
-const mainNews = data.items.slice(0, 3);
-const smallNews = data.items.slice(3, 12);
-
-const mainNewsContainer = document.querySelector('.articles__big-column');
-const smallNewsContainer = document.querySelector('.articles__small-column');
+let data = null;
 
 const escapeString = (string) => {
-	const symbols = {
-	'&': '&amp;',
-	'<': '&lt;',
-	'>': '&gt;'
+	const tagsToReplace = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;'
 	};
-	return string.replace(/[&<>]/g, (tag) => {
-		
-		return symbols[tag] || tag;
-	})
+	
+	return string.replace(/[&<>]/g, function(tag) {
+		return tagsToReplace[tag] || tag;
+	});
 }
 
+const createMainNewsItem = (item) => {
+	return `
+    <article class="main-article">
+        <div class="main-article__image-container">
+            <img class="article-img main-article__img" src="${encodeURI(item.image)}" alt="Фото новости">
+        </div>
+        <div class="main-article__content">
+            <span class="article-category">${escapeString(data.categories.find(({ id }) => item.category_id === id).name)}</span>
+            <h2 class="main-article__title">${escapeString(item.title)}</h2>
+            <p class="main-article__text">${escapeString(item.description)}</p>
+            <span class="article-source main-article__caption">${escapeString(data.sources.find(({ id }) => item.source_id === id).name)}</span>
+        </div>
+    </article>
+    `;
+}
 
+const createSmallNewsItem = (item) => {
+	return `
+    <article class="small-article">
+        <h2 class="small-article__title">${escapeString(item.title)}</h2>
+        <span class="article-date">${escapeString(data.sources.find(({ id }) => item.source_id === id).name)}</span>
+        <span class="article-source">${escapeString(new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric' }))}</span>
+    </article>
+    `;
+}
 
-mainNews.forEach((item) => {
-	const template = document.createElement('template');
-	const categoryData = data.categories.find((categoryItem) => categoryItem.id === item.category_id);
-	const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
-	
-	template.innerHTML =`
-	<article class="main-article">
-	<div class="main-article__image-container">
-				<img class="main-article__image" src = "${encodeURI(item.image)}" alt = "Фото новости">
-		</div>
-		<div class="main-article__content">
-				<span class="article-category main-article__category">${escapeString(categoryData.name)}</span>
-				<h2 class="main-article__title">${escapeString(item.title)}</h2>
-				<p class="main-article__text">${escapeString(item.description)}</p>
-				<span class="article-source main-article__source">${escapeString(sourceData.name)}</span>
-		</div>
-</article>
-`;
-	
-	mainNewsContainer.appendChild(template.content);
-});
+const renderNews = (categoryId) => {
+	fetch('https://frontend.karpovcourses.net/api/v2/ru/news/' + (categoryId ? categoryId : ''))
+		.then(response => response.json())
+		.then((responseJson) => {
+			data = responseJson;
+			const mainNews = data.items.slice(0, 3);
+			const mainNewsContainer = document.querySelector('.articles__big-column');
+			
+			mainNews.forEach((item) => {
+				const template = document.createElement('template');
+				template.innerHTML = createMainNewsItem(item);
+				mainNewsContainer.appendChild(template.content);
+			})
+			
+			const smallNews = data.items.slice(3, 12);
+			const smallNewsContainer = document.querySelector('.articles__small-column');
+			
+			smallNews.forEach((item) => {
+				const template = document.createElement('template');
+				template.innerHTML = createSmallNewsItem(item);
+				smallNewsContainer.appendChild(template.content);
+			})
+		})
+}
 
-smallNews.forEach((item) => {
-	const template = document.createElement('template');
-	const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
-	const dateData = new Date(item.date).toLocaleDateString('ru-RU', {month: 'long', day: "numeric"});
-	
-	template.innerHTML =`
-	<article class="small-article">
-		<h2 class="small-article__title">${escapeString(item.title)}</h2>
-		<p class="small-article__caption">
-			<span class="article-date small-article__date">${dateData}</span>
-			<span class="article-source small-article__source">${escapeString(sourceData.name)}</span>
-		</p>
-	</article>
-	`;
-	smallNewsContainer.appendChild(template.content);
-});
 
 
 
